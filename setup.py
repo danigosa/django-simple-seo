c27b1cf536a99765ac92fb92e13022ed6ac0a455
danigosa <dani.gonzalez@infantium.com>
2014-08-07 20:53:06 +0200

    minors

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-simple-seo',
     version='0.2.0',
-    packages=['simple-seo'],
+    packages=['simple_seo'],
     include_package_data=True,
     license='MIT License',  # example license
     description='Simple SEO Module for Django',
